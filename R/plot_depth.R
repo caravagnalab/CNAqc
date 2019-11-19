44c023f3604d8cf2e44919c01ed884e00be356c5
Williams <william1@lbst0129.mskcc.org>
2019-11-19 12:25:26 -0500

    Added optional chromosome annotations to depth plot

--- R/plot_depth.R
+++ R/plot_depth.R
@@ -6,6 +6,7 @@
 #' @param x An object of class \code{cnaqc}, created by the \code{init} function.
 #' @param N Mutations to use, randomly sampled.
 #' @param chromosomes The chromosome to use for this plot.
+#' @param annotate_chromosomes Boolean value specifying if chromosome should be annotated or not, default = FALSE
 #'
 #' @return A \code{ggplot} object.
 #' @export
@@ -19,7 +20,7 @@
 #' plot_depth(x, N = 100)
 #' plot_depth(x, N = 1000)
 #' plot_depth(x, N = 10000)
-plot_depth = function(x, N = 5000, chromosomes = paste0('chr', c(1:22, 'X', 'Y')))
+plot_depth = function(x, N = 5000, chromosomes = paste0('chr', c(1:22, 'X', 'Y')), annotate_chromosomes = FALSE)
 {
   stopifnot(inherits(x, 'cnaqc'))
 
@@ -43,15 +44,21 @@ plot_depth = function(x, N = 5000, chromosomes = paste0('chr', c(1:22, 'X', 'Y')
   if(nrow(mutations) > N)
     mutations = mutations %>% sample_n(N)
 
-  maxY = max(mutations$DP) * .9
+  maxY = round(max(mutations$DP)  / 20) * 20
+  minY = floor(min(mutations$DP)  / 20) * 20
   label_maxY = paste0("N = ", N, ' (', round(N/N_all * 100), '%)')
-
-  ggplot(mutations,
-                  aes(x = from, y = DP)) +
+  
+  base_plot = ggplot()
+  if (annotate_chromosomes == TRUE) {
+    base_plot = blank_genome(chromosomes, y = minY)  
+  }
+  
+  depthplot = base_plot +
     scale_fill_viridis_c() +
-    geom_point(size = .05) +
+    geom_point(data = mutations, aes(x = from, y = DP), size = .05) +
     my_ggplot_theme() +
     xlim(low, upp) +
+    ylim(c(minY, maxY)) +
     theme(
       axis.text.x = element_blank(),
       axis.ticks.x = element_blank(),
@@ -60,5 +67,7 @@ plot_depth = function(x, N = 5000, chromosomes = paste0('chr', c(1:22, 'X', 'Y')
     labs(y = "DP") +
     geom_hline(yintercept = med_DP, size = .4, linetype = 'dashed', color = 'darkred') +
     guides(fill = FALSE) +
-    annotate("label", fill = 'white', x = upp, y = maxY, label = label_maxY, size = 2, hjust = 1)
+    annotate("label", fill = 'white', x = upp, y = maxY * 0.9, label = label_maxY, size = 2, hjust = 1)
+  
+  depthplot
 }
