ec8466ea7a3ac88959b83b2b9c4069da00d9f939
caravagn <gcaravagn@gmail.com>
2020-02-04 15:51:38 +0000

    Update eplot.R

--- R/eplot.R
+++ R/eplot.R
@@ -1,7 +1,17 @@
 # Empty plot
 eplot = function()
 {
-  geom_text(data = data.frame(x = 0, y = 0, label = "X"), aes(x=x,y=y,label=label)) +
-    theme_void()  +
-    theme(panel.border = element_rect(colour = "black", fill = NA, size=1, linetype = 'dashed'))
+  ggplot(data = data.frame(x = 0, y = 0, label = "X"),
+            aes(x = x, y = y, label = label)) +
+    # theme_void()  +
+    theme(panel.border = element_rect(
+      colour = "black",
+      fill = NA,
+      size = 1,
+      linetype = 'dashed'
+    ),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.line = element_blank()) 
 }
