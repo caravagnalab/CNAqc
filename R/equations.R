368d70d89f00b8827d88e58bf5692dfe4c03337b
Giulio Caravagna <gcaravagn@gmail.com>
2020-02-14 09:39:42 +0000

    Update equations.R

--- R/equations.R
+++ R/equations.R
@@ -1,3 +1,12 @@
+#
+expected_vaf_fun = function(m, M, mut.allele, p)
+{
+  P = m+M
+
+  expected_mutant_reads = mut.allele * p
+  expected_sequencing_depth = 2 * (1 - p) + p * P
+  expected_mutant_reads / expected_sequencing_depth
+}
 
 # Compute CCF values from mutation multiplicity
 # m - minor allele
@@ -7,7 +16,7 @@
 ccf_adjustment_fun = function(v, m, M, p, mut.allele = 1)
 {
   CN = m+M
-  
+
   v * ((CN-2) * p + 2) / (mut.allele * p)
 }
 
@@ -20,7 +29,7 @@ ccf_adjustment_fun = function(v, m, M, p, mut.allele = 1)
 purity_estimation_fun = function(v, m, M, p, mut.allele = 1)
 {
   CN = m+M
-  
+
   (2 * v)/(v * (2-CN) + m)
 }
 
