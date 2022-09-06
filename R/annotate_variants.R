f791a05266948a1a75dcb835dc9355160e110690
Giulio Caravagna <gcaravagn@gmail.com>
2022-09-06 18:24:54 +0200

    Almost final version

--- R/annotate_variants.R
+++ R/annotate_variants.R
@@ -1,22 +1,26 @@
-#' Annotate variant with VariantAnnotation.
+#' Annotate variants and drivers.
 #'
-#' @description  The function provides an easy and fast way to annotate variants,
-#' reporting the locations of the different mutations and the consequences for
-#' those in coding regions. Then putative drivers are returned upon matching
-#' against lists of known drivers.
+#' @description  Easy and fast way to annotate input mutations, and detect
+#' potential driver mutations. This function computes the locations of
+#' the different mutations and the consequences of substituions mapped to
+#' coding regions, using \href{https://bioconductor.org/packages/release/bioc/html/VariantAnnotation.html}{VariantAnnotation} and other Bioconductor packages.
+#' Then, putative drivers are annotated upon matching from an input list that,
+#' by default, is compiled from the \href{https://www.intogen.org}{Intogen} database. Drivers are selected among
+#' coding substituions with known effect.
 #'
-#' @param x A data.frame with columns c("chr", "from", "to"), or a CNAqc object.
-#' @param driver_list a one column data.frame with gene symbols.
-#' @param collapse whether or not to merge different location, consequences and genes in a single string or split the mutations
-#' @param filter_tumor_type filter Intogen drivers by tumor type.
+#' @param x A CNAqc object.
+#' @param drivers A dataframe in the format of the `intogen_drivers` one released with
+#' CNAqc. In particular, it must contain a column named `gene` to identify gene names.
 #'
-#' @return a data.frame with an additional columns `location` reporting the position of the variant in the genome (`coding`, `intron`, `threeUTR`, ...),
-#' `consequence` with the consequence of coding mutations (`synonymous`, `nonsynonymous`, ...), `is_driver` a boolean that indicates if the gene is a driver,
-#' `gene_symbol` for the annotated corresponding gene symbol (if the variant is in a gene) and `driver_label` with the driver label written as
-#' `gene_refAA->varAA` (is `NA` in case `is_driver = FALSE`).
+#' @return A CNAqc object with variants annotated. For each variant this object contains:
 #'
-#' The `chr` column in the input should contain the chr prefix before the chromosome number. The annotation process is based on the package
-#' \href{https://bioconductor.org/packages/release/bioc/html/VariantAnnotation.html}{VariantAnnotation}.
+#' - `location` reporting the position of the variant in the genome (`coding`, `intron`, `threeUTR`, ...),
+#' - `consequence` with the consequence of coding mutations (`synonymous`, `nonsynonymous`, ...),
+#' - `is_driver` a boolean that indicates if the gene is a driver,
+#' - `gene_symbol` for the annotated corresponding gene symbol (if the variant is in a gene)
+#' - `driver_label` with the driver label written as `gene_refAA->varAA` (`NA` in case `is_driver = FALSE`).
+#'
+#' The annotation process is based on the package \href{https://bioconductor.org/packages/release/bioc/html/VariantAnnotation.html}{VariantAnnotation}.
 #'
 #' @export
 #'
@@ -33,13 +37,14 @@
 #' }
 #'
 annotate_variants <- function(x,
-                              ref = "hg19",
-                              driver_list = CNAqc::intogen_drivers,
-                              collapse = TRUE,
-                              filter_tumor_type = NULL)
+                              drivers = CNAqc::intogen_drivers)
 {
-  tx_pkg <-  paste0("TxDb.Hsapiens.UCSC.", ref, ".knownGene")
-  bs_pkg <- paste0("BSgenome.Hsapiens.UCSC.", ref)
+  # Old params, now fixed.
+  collapse = TRUE
+
+  inputs = annotate_variants_preprocess(x)
+  mutations = inputs$mutations
+  reference = inputs$reference
 
   # Check for available packages
   rqp = function(x) {
@@ -49,38 +54,21 @@ annotate_variants <- function(x,
   }
 
   # Required packages
+  tx_pkg <-  paste0("TxDb.Hsapiens.UCSC.", reference, ".knownGene")
+  bs_pkg <- paste0("BSgenome.Hsapiens.UCSC.", reference)
+
   tx_pkg %>% rqp()
   bs_pkg %>% rqp()
   "Organism.dplyr" %>% rqp()
   "org.Hs.eg.db" %>% rqp()
 
-  # Define input
-  inp = x
-
-  if (inherits(x, 'cnaqc'))
-  {
-    cli::cli_alert_info("Extracting mutations from a CNAqc object.")
-    inp = x %>% Mutations()
-  }
-
-  # Cancel other annotations
-  if(
-    any(c('is_driver', "driver_label") %in% colnames(inp))
-  )
-  {
-    cli::cli_alert_warning("There are existing annotations in your data, they will be cancelled.")
-    print(inp %>% filter(is_driver))
-
-    inp$is_driver = inp$driver_label = NULL
-  }
-
   # Get data and ranges
   txdb <- eval(parse(text = tx_pkg))
-  inp <- inp %>% mutate(to = to - 1)
+  mutations <- mutations %>% mutate(to = to - 1)
 
   rd <-
     GenomicRanges::makeGRangesFromDataFrame(
-      inp,
+      mutations,
       start.field = "from",
       end.field = "to",
       seqnames.field = "chr",
@@ -134,24 +122,25 @@ annotate_variants <- function(x,
               .groups = 'keep') %>%
     ungroup()
 
-  input_coding <-
-    dplyr::left_join(loc_df %>%  filter(grepl(location, pattern = "coding")),
-                     inp,
+  mutationsut_coding <-
+    dplyr::left_join(loc_df %>%
+                       filter(grepl(location, pattern = "coding")),
+                     mutations,
                      by = c("chr", "from", "to"))
 
-  # inp %>% filter(chr == 'chr1', from == 985357)
-
-  input_coding <-  dplyr::left_join(
-    input_coding,
-    seqlengths(Hsapiens) %>%  as.data.frame() %>%
-      tibble::rownames_to_column() %>% dplyr::rename(chr = "rowname", length = "."),
+  mutationsut_coding <-  dplyr::left_join(
+    mutationsut_coding,
+    seqlengths(Hsapiens) %>%
+      as.data.frame() %>%
+      tibble::rownames_to_column() %>%
+      dplyr::rename(chr = "rowname", length = "."),
     by = "chr"
   ) %>%
     filter(from < length)
 
-  input_coding_grange <-
+  mutationsut_coding_grange <-
     GenomicRanges::makeGRangesFromDataFrame(
-      input_coding,
+      mutationsut_coding,
       start.field = "from",
       end.field = "to",
       seqnames.field = "chr",
@@ -163,20 +152,12 @@ annotate_variants <- function(x,
   cli::cli_process_start("Predicting coding")
 
   coding <-
-    VariantAnnotation::predictCoding(input_coding_grange,
+    VariantAnnotation::predictCoding(mutationsut_coding_grange,
                                      txdb,
                                      seqSource = Hsapiens,
-                                     DNAStringSetList(lapply(input_coding$alt, function(i)
+                                     DNAStringSetList(lapply(mutationsut_coding$alt, function(i)
                                        i)) %>% unlist)
-  cli::cli_process_done()
 
-  if (!is.null(filter_tumor_type)) {
-    driver_list <-
-      driver_list %>% filter(CANCER_TYPE %in% filter_tumor_type)
-  }
-
-  colnames(driver_list)[1] <-  "gene_symbol"
-  driver_list$is_driver <-  TRUE
   output_coding <- as.data.frame(coding) %>%
     dplyr::mutate(
       segment_id = paste(seqnames, start, end, sep = ":"),
@@ -187,64 +168,321 @@ annotate_variants <- function(x,
       refAA = REFAA,
       varAA = VARAA
     ) %>%
-    dplyr::select(chr, from, to, consequence, refAA, varAA) %>% unique()
+    dplyr::select(chr, from, to, consequence, refAA, varAA) %>%
+    unique()
+
+  cli::cli_h3("Coding substitutions found")
+  print(output_coding)
+  cat("\n")
+
+  cli::cli_process_done()
+
+  # Drivers annotation
+  cli::cli_process_start("Drivers annotation")
+
+  if(!is.data.frame(drivers)) stop("`drivers` has to be a dataframe ")
+
+  driver_genes = drivers$gene %>% unique
 
   res <-
-    dplyr::left_join(loc_df, output_coding,  by = c("chr", "from", "to")) %>% mutate(
-      driver_label = paste0(gene_symbol, "_", refAA, "->", varAA)
-    )
-  res <-
-    dplyr::left_join(res,
-                     driver_list %>% dplyr::select(gene_symbol, is_driver),
-                     by = "gene_symbol")
-
-  res <-  res %>%
+    dplyr::left_join(loc_df, output_coding,  by = c("chr", "from", "to")) %>%
+    mutate(
+      driver_label = paste0(gene_symbol, "_", refAA, "->", varAA),
+      is_driver = gene_symbol %in% driver_genes
+    ) %>%
     mutate(is_driver =
              ifelse(
                is.na(is_driver) |
                  !grepl(location, pattern = "coding") |
                  is.na(consequence) |
                  grepl(consequence, pattern = "^(?!non)synonymous", perl = T),
-      FALSE,
-      TRUE)
+               FALSE,
+               gene_symbol %in% driver_genes # check against the list
+               )
     ) %>%
-    mutate(driver_label = ifelse(is_driver, driver_label, NA)) %>%  unique()
+    mutate(driver_label = ifelse(is_driver, driver_label, NA)) %>%
+    unique()
 
   cli::cli_h3("Found {.green {sum(res$is_driver)}} driver(s)")
+
   print(res %>% filter(is_driver))
 
+  cli::cli_process_done()
 
   if(collapse){
-    res <- res %>%  group_by(chr,from,to) %>%
+    res <- res %>%
+      group_by(chr,from,to) %>%
       summarize(gene_symbol = paste(unique(gene_symbol), collapse = ":"),
                 location = paste(unique(location), collapse = ":"),
                 refAA = paste(unique(refAA), collapse = ":"),
                 varAA = paste(unique(varAA), collapse = ":"),
                 consequence = paste(unique(consequence), collapse = ":"),
                 driver_label= paste(unique(driver_label), collapse = ":"),
-                is_driver = any(is_driver))
+                is_driver = any(is_driver)
+                ) %>%
+      ungroup()
+
     res$driver_label <- gsub(res$driver_label, pattern = "NA:", replacement = "", fixed = TRUE)
   }
 
-
+  # Re-assembly CNAqc obect
   final_table = left_join(
-    inp %>% mutate(to = to + 1),
+    mutations %>% mutate(to = to + 1),
     res %>% mutate(to = to + 1),
     by = c("chr", "from", "to")
-    ) %>%
+  ) %>%
     dplyr::arrange(-is_driver)
 
+  x_new = CNAqc::init(
+    mutations = final_table,
+    cna = x %>% CNA(),
+    purity = x$purity,
+    ref = x$reference_genome
+  )
 
+  return(x_new)
+}
 
-  # final_table %>% filter(is_driver)
-  # res %>% filter(is_driver) %>% mutate(to = to + 1)
-  # inp %>% filter(Hugo_Symbol =='VHL')%>% mutate(to = to + 1) %>% dplyr::select(chr, from, to, ref, alt)
+annotate_variants_preprocess = function(x)
+{
+  cli::cli_process_start("Preparing mutationsut data")
 
-  if (inherits(x, 'cnaqc'))
+  # Define mutationsuts
+  mutations = x %>% Mutations()
+  reference = x$reference_genome
+
+  # Cancel other annotations
+  if(
+    any(c('is_driver', "driver_label") %in% colnames(mutations))
+  )
   {
-    x$mutations = final_table
-    final_table = x
+    cli::cli_alert_warning("Existing annotations in your data will be cancelled.")
+    cat("\n")
+    print(mutations %>% dplyr::filter(is_driver))
+    cat("\n")
+
+    mutations$is_driver = mutations$driver_label = NULL
   }
 
-  return(final_table)
+  cli::cli_process_done()
+
+  return(list(mutations = mutations, reference = reference))
 }
+
+# annotate_variants <- function(x,
+#                               ref = "hg19",
+#                               driver_list = CNAqc::intogen_drivers,
+#                               collapse = TRUE,
+#                               filter_tumor_type = NULL)
+# {
+#   tx_pkg <-  paste0("TxDb.Hsapiens.UCSC.", ref, ".knownGene")
+#   bs_pkg <- paste0("BSgenome.Hsapiens.UCSC.", ref)
+#
+#   # Check for available packages
+#   rqp = function(x) {
+#     if (!require(x,  character.only = T, quietly = T) %>% suppressWarnings()) {
+#       cli::cli_abort("Bioconducator package {.field {x}} is required to annotate variants")
+#     }
+#   }
+#
+#   # Required packages
+#   tx_pkg %>% rqp()
+#   bs_pkg %>% rqp()
+#   "Organism.dplyr" %>% rqp()
+#   "org.Hs.eg.db" %>% rqp()
+#
+#   # Define mutationsut
+#   mutations = x
+#
+#   if (inherits(x, 'cnaqc'))
+#   {
+#     cli::cli_alert_info("Extracting mutations from a CNAqc object.")
+#     mutations = x %>% Mutations()
+#   }
+#
+#   # Cancel other annotations
+#   if(
+#     any(c('is_driver', "driver_label") %in% colnames(mutations))
+#   )
+#   {
+#     cli::cli_alert_warning("There are existing annotations in your data, they will be cancelled.")
+#     print(mutations %>% filter(is_driver))
+#
+#     mutations$is_driver = mutations$driver_label = NULL
+#   }
+#
+#   # Get data and ranges
+#   txdb <- eval(parse(text = tx_pkg))
+#   mutations <- mutations %>% mutate(to = to - 1)
+#
+#   rd <-
+#     GenomicRanges::makeGRangesFromDataFrame(
+#       mutations,
+#       start.field = "from",
+#       end.field = "to",
+#       seqnames.field = "chr",
+#       keep.extra.columns = F
+#     )
+#
+#   # VariantAnnotation package
+#   cli::cli_process_start("Locating variants with {crayon::yellow('VariantAnnotation')}")
+#
+#   loc <-
+#     VariantAnnotation::locateVariants(rd, txdb, VariantAnnotation::AllVariants())
+#
+#   cli::cli_process_done()
+#
+#   # Src organism DB
+#   cli::cli_process_start("Traslating Entrez ids")
+#
+#   src <- src_organism(tx_pkg)
+#
+#   translated_enttrz <-
+#     AnnotationDbi::select(
+#       src,
+#       keys = loc$GENEID,
+#       columns = c("symbol"),
+#       keytype = "entrez"
+#     )
+#
+#   cli::cli_process_done()
+#
+#   cli::cli_process_start("Transforming data")
+#
+#   map <-
+#     setNames(translated_enttrz$entrez, object =  translated_enttrz$symbol)
+#
+#   loc$gene_symbol <- map[loc$GENEID]
+#
+#   loc_df <- as.data.frame(loc) %>%
+#     as_tibble() %>%
+#     dplyr::mutate(
+#       segment_id = paste(seqnames, start, end, sep = ":"),
+#       chr = seqnames,
+#       from = start,
+#       to = end,
+#       location = LOCATION
+#     ) %>%
+#     dplyr::select(chr, from, to, location, gene_symbol) %>%
+#     dplyr::filter(gene_symbol != "NA") %>%
+#     unique() %>%
+#     group_by(chr, from, to, gene_symbol) %>%
+#     summarize(location = paste(location, collapse = ":"),
+#               .groups = 'keep') %>%
+#     ungroup()
+#
+#   mutationsut_coding <-
+#     dplyr::left_join(loc_df %>%  filter(grepl(location, pattern = "coding")),
+#                      mutations,
+#                      by = c("chr", "from", "to"))
+#
+#   # mutations %>% filter(chr == 'chr1', from == 985357)
+#
+#   mutationsut_coding <-  dplyr::left_join(
+#     mutationsut_coding,
+#     seqlengths(Hsapiens) %>%  as.data.frame() %>%
+#       tibble::rownames_to_column() %>% dplyr::rename(chr = "rowname", length = "."),
+#     by = "chr"
+#   ) %>%
+#     filter(from < length)
+#
+#   mutationsut_coding_grange <-
+#     GenomicRanges::makeGRangesFromDataFrame(
+#       mutationsut_coding,
+#       start.field = "from",
+#       end.field = "to",
+#       seqnames.field = "chr",
+#       keep.extra.columns = F
+#     )
+#
+#   cli::cli_process_done()
+#
+#   cli::cli_process_start("Predicting coding")
+#
+#   coding <-
+#     VariantAnnotation::predictCoding(mutationsut_coding_grange,
+#                                      txdb,
+#                                      seqSource = Hsapiens,
+#                                      DNAStringSetList(lapply(mutationsut_coding$alt, function(i)
+#                                        i)) %>% unlist)
+#   cli::cli_process_done()
+#
+#   if (!is.null(filter_tumor_type)) {
+#     driver_list <-
+#       driver_list %>% filter(CANCER_TYPE %in% filter_tumor_type)
+#   }
+#
+#   colnames(driver_list)[1] <-  "gene_symbol"
+#   driver_list$is_driver <-  TRUE
+#   output_coding <- as.data.frame(coding) %>%
+#     dplyr::mutate(
+#       segment_id = paste(seqnames, start, end, sep = ":"),
+#       chr = seqnames,
+#       from = start,
+#       to = end,
+#       consequence = CONSEQUENCE,
+#       refAA = REFAA,
+#       varAA = VARAA
+#     ) %>%
+#     dplyr::select(chr, from, to, consequence, refAA, varAA) %>% unique()
+#
+#   res <-
+#     dplyr::left_join(loc_df, output_coding,  by = c("chr", "from", "to")) %>% mutate(
+#       driver_label = paste0(gene_symbol, "_", refAA, "->", varAA)
+#     )
+#   res <-
+#     dplyr::left_join(res,
+#                      driver_list %>% dplyr::select(gene_symbol, is_driver),
+#                      by = "gene_symbol")
+#
+#   res <-  res %>%
+#     mutate(is_driver =
+#              ifelse(
+#                is.na(is_driver) |
+#                  !grepl(location, pattern = "coding") |
+#                  is.na(consequence) |
+#                  grepl(consequence, pattern = "^(?!non)synonymous", perl = T),
+#       FALSE,
+#       TRUE)
+#     ) %>%
+#     mutate(driver_label = ifelse(is_driver, driver_label, NA)) %>%  unique()
+#
+#   cli::cli_h3("Found {.green {sum(res$is_driver)}} driver(s)")
+#   print(res %>% filter(is_driver))
+#
+#
+#   if(collapse){
+#     res <- res %>%  group_by(chr,from,to) %>%
+#       summarize(gene_symbol = paste(unique(gene_symbol), collapse = ":"),
+#                 location = paste(unique(location), collapse = ":"),
+#                 refAA = paste(unique(refAA), collapse = ":"),
+#                 varAA = paste(unique(varAA), collapse = ":"),
+#                 consequence = paste(unique(consequence), collapse = ":"),
+#                 driver_label= paste(unique(driver_label), collapse = ":"),
+#                 is_driver = any(is_driver))
+#     res$driver_label <- gsub(res$driver_label, pattern = "NA:", replacement = "", fixed = TRUE)
+#   }
+#
+#
+#   final_table = left_join(
+#     mutations %>% mutate(to = to + 1),
+#     res %>% mutate(to = to + 1),
+#     by = c("chr", "from", "to")
+#     ) %>%
+#     dplyr::arrange(-is_driver)
+#
+#
+#
+#   # final_table %>% filter(is_driver)
+#   # res %>% filter(is_driver) %>% mutate(to = to + 1)
+#   # mutations %>% filter(Hugo_Symbol =='VHL')%>% mutate(to = to + 1) %>% dplyr::select(chr, from, to, ref, alt)
+#
+#   if (inherits(x, 'cnaqc'))
+#   {
+#     x$mutations = final_table
+#     final_table = x
+#   }
+#
+#   return(final_table)
+# }
+
--- nobuild/data.R
+++ nobuild/data.R
@@ -1,3 +1,22 @@
+#' Coordinates for GRCh38 genes
+#'
+#' @docType data
+#'
+#' @usage data(gene_coordinates_GRCh38)
+#'
+#' @format A tibble that represents the coordinates for genes in the GRCh38 genome assembly,
+#' reporting the gene symbol, from and to (chromosome range).
+#'
+#' @keywords datasets
+#'
+#' @references GRCh38
+#'
+#' @examples
+#' data(gene_coordinates_GRCh38)
+#' gene_coordinates_GRCh38
+#'
+"gene_coordinates_GRCh38"
+
 #' Coordinates for hg19 chromosomes.
 #'
 #' @docType data
@@ -76,6 +95,25 @@
 #'
 "example_PCAWG"
 
+#' Driver mutations released with the DriverDBv3 database
+#' \url{http://driverdb.tms.cmu.edu.tw/}
+#'
+#' @docType data
+#'
+#' @usage data(DriverDBv3)
+#'
+#' @format A tibble.
+#'
+#' @keywords datasets
+#'
+#' @references ??
+#'
+#' @examples
+#' data(DriverDBv3)
+#' DriverDBv3
+#'
+"DriverDBv3"
+
 #' Data (simulation performance) from the trainig set to auto-tune epsilon.
 #'
 #' @docType data
@@ -93,3 +131,26 @@
 #' fpr_test
 #'
 "fpr_test"
+
+#' Driver mutations released with the boost DM method
+#' \url{https://www.intogen.org/boostdm/}
+#'
+#' @docType data
+#'
+#' @usage data(boostDM_drivers)
+#'
+#' @format A tibble.
+#'
+#' @keywords datasets
+#'
+#' @references Muiños, F., Martínez-Jiménez, F., Pich, O. et al.
+#' In silico saturation mutagenesis of cancer genes. Nature 596, 428–432 (2021).
+#' https://doi.org/10.1038/s41586-021-03771-1
+#'
+#' @examples
+#' data(boostDM_drivers)
+#' boostDM_drivers
+#'
+"boostDM_drivers"
+
+
