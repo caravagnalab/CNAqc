e321e0034d96a2b6eafe12d9253e9658837645bf
Giulio Caravagna <gcaravagn@gmail.com>
2022-09-06 19:09:30 +0200

    Update get_reference.R

--- R/get_reference.R
+++ R/get_reference.R
@@ -1,10 +1,11 @@
 get_reference = function(ref)
 {
+
   if(ref %in% c("hg19", "GRCh37"))
     return(CNAqc::chr_coordinates_hg19)
-  
+
   if(ref %in% c("hg38", "GRCh38"))
     return(CNAqc::chr_coordinates_GRCh38)
-   
+
   stop("Available references: hg19 (or GRCh37), and hg38 (or GRCh38)")
 }
