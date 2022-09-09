9d0cccad26aecc0adf9c2162ad260e8e9ef099df
Salvatore Milite <militesalvatore@gmail.com>
2022-09-09 14:37:22 +0200

    Update data.R

--- R/data.R
+++ R/data.R
@@ -11,7 +11,7 @@
 #' @keywords datasets
 #'
 #' @references hg19
-#'
+#' @export
 #' @examples
 #' data(chr_coordinates_hg19)
 #' chr_coordinates_hg19
@@ -29,7 +29,7 @@
 #' centromers.
 #'
 #' @keywords datasets
-#'
+#' @export
 #' @references GRCh38
 #'
 #' @examples
@@ -69,7 +69,7 @@
 #' @references The ICGC/TCGA Pan-Cancer Analysis of Whole Genomes Consortium.
 #' Pan-cancer analysis of whole genomes. Nature 578, 82–93 (2020).
 #'  https://doi.org/10.1038/s41586-020-1969-6
-#'
+#' @export
 #' @examples
 #' data(example_PCAWG)
 #' example_PCAWG
@@ -87,7 +87,7 @@
 #' @keywords datasets
 #'
 #' @references
-#'
+#' @export
 #' @examples
 #' data(fpr_test)
 #' fpr_test
@@ -105,7 +105,7 @@
 #' @keywords datasets
 #'
 #' @references
-#'
+#' @export
 #' @examples
 #' data(intogen_drivers)
 #' intogen_drivers
